e91c2b12b415dff71e365c13861cd2d093187ccd
Elijah Roussos <elijah.rou@gmail.com>
2019-07-08 10:38:17 +0200

    Add python files for concept learning

--- src/main.py
+++ src/main.py
@@ -1,2 +1,10 @@
+# Elijah Roussos
+# Script to analyse Harambee data via Naive Bayes & Candidate Elimination
+
+## Imports
 import numpy as np
 import pandas as pd
+ 
+## Import Data
+dataset = pd.read_csv("data/raw/teaching_training_data.csv")
+
