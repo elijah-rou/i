7a40a6f75835deed83ebd33b111eac6eca1d2b61
darren878 <52186672+darren878@users.noreply.github.com>
2019-07-25 09:14:33 +0200

    Add files via upload
    new models

--- Part2.R
+++ Part2.R
@@ -100,7 +100,70 @@ model_rpart$results
 # Confusion Matrix
 table(predicted, testData$loe_morethan6months)
 
-# Section C: XGBoost Model
+# Section C: Logistic Regression, SVM
+df_3i <- select(df_2, -c(X.1, X, survey_date_month, survey_num, working, job_start_date, job_leave_date, financial_situation_now, financial_situation_5years, age, fin_situ_now, fin_situ_future, com_score, num_score, company_size, monthly_pay, length_of_employment, peoplelive_15plus, province, dob))
+df_3i <- df_3i %>% filter(!is.na(volunteer), !is.na(leadershiprole), !is.na(peoplelive), !is.na(anygrant), !is.na(anyhhincome), !is.na(givemoney_yes))
+df_3i <- mutate(df_3i,
+               loe_morethan6months = factor(loe_morethan6months),
+               anygrant = factor(anygrant),
+               anyhhincome = factor(anygrant),
+               givemoney_yes = factor(anygrant),
+               numchildren = as.numeric(numchildren),
+               numearnincome = as.numeric(numearnincome))
+
+# Impute numeric variables (e.g. age and test scores)
+preProcess_missingdata_model <- preProcess(df_3i, method="knnImpute")
+df_3i <- predict(preProcess_missingdata_model, newdata = df_3i)
+
+# Split into training and test data
+df_train_index <- df_3i %>%
+  select(unid) %>% 
+  sample_frac(0.7)
+df_train <- left_join(df_train_index, df_3i)
+df_test <- anti_join(df_3i, df_train_index)
+
+# Logistic Regression
+logistic <- glm(loe_morethan6months ~ gender + age_at_survey + opt_score + grit_score, data=df_train, family="binomial")
+predicted_log=predict(logistic,df_test, type="response")
+predicted_log <- ifelse(predicted_log>0.5,1,0)
+table(predicted_log, df_test$loe_morethan6months)
+
+# SVM
+library(e1071)
+fit <- svm(loe_morethan6months ~ gender + age_at_survey + fin_situ_change, data=df_train)
+predicted_svm <- predict(fit, df_test)
+table(predicted_svm, df_test$loe_morethan6months)
+
+# Naive Bayes
+fit_nb <- naiveBayes(loe_morethan6months ~ gender + age_at_survey + opt_score + grit_score, data=df_train)
+predicted_nb <- predict(fit_nb, df_test)
+table(predicted_nb, df_test$loe_morethan6months)
+
+# KNN
+install.packages("kknn")
+library(kknn)
+library(Metrics)
+fit_knn <- kknn(loe_morethan6months ~ age_at_survey + grit_score + opt_score + grit_score + numearnincome, df_train, df_test, k = 5, kernel = "rectangular", distance = 2)
+predicted_knn <- predict(fit_knn)
+table(predicted_knn, df_test$loe_morethan6months)
+
+# Random Forest
+install.packages("randomForest")
+library(randomForest)
+fit_rf <- randomForest(loe_morethan6months ~ gender + age_at_survey + fin_situ_change + opt_score, df_train, ntree=500)
+predicted_rf <- predict(fit_rf,df_test)
+table(predicted_rf, df_test$loe_morethan6months)
+
+# GBM
+install.packages("gbm")
+library(caret)
+fitControl <- trainControl(method = "repeatedcv", number=4, repeats=4)
+fit_gbm <- train(loe_morethan6months ~ gender + age_at_survey, data=df_train, method="gbm", trControl=fitControl, verbose=FALSE)
+predicted_gbm = predict(fit_gbm, df_test, type="prob")[,2]
+predicted_gbm <- ifelse(predicted_gbm>0.5,1,0)
+table(predicted_gbm, df_test$loe_morethan6months)
+
+# Section D: XGBoost Model
 install.packages("tidyverse")
 install.packages("RANN")
 install.packages("caret")
@@ -144,36 +207,37 @@ testData2 <- df_4[-trainRowNumbers2,]
 
 # Loading labels of train data
 labels = trainData2 ['loe_morethan6months']
-trainData2 = trainData2 [-grep('loe_morethan6months', colnames(trainData2))]
+trainData2i = trainData2 [-grep('loe_morethan6months', colnames(trainData2))]
 
 # Combine train and test data
 df_all <- df_4[-grep('loe_morethan6months',colnames(df_4))]
-X = df_all[df_all$unid %in% trainData2$unid,]
+X = df_all[df_all$unid %in% trainData2i$unid,]
 X_test = df_all[df_all$unid %in% testData2$unid,]
 
 # Converting labels into a matrix
 mat_y <- as.matrix(labels)
 xgb <- xgboost(data = data.matrix(X[,-1]),
                label = mat_y,
-               eta = 0.3,
-               max_depth = 15,
+               eta = 0.025,
+               max_depth = 10,
                nround=100,
                objective = "binary:logistic",
                booster = "gbtree",
                subsample = 0.8,
-               scale_pos_weight = 0.5
+               scale_pos_weight = 0.5,
+               colsample_bytree = 1,
+               min_child_weight = 1
 )
 
 #Testing the results of our model
 z_pred <- predict(xgb, data.matrix(X_test[,-1]))
 z_pred <- ifelse(z_pred>0.5,1,0)
-summary(z_pred)
-table(z_pred)
 table(z_pred, testData$loe_morethan6months)
 
+nrounds = 1000
 # Tuning the parameters
 tune_grid <- expand.grid(
-  nrounds = seq(from = 200, to = nrounds),
+  nrounds = seq(from = 200, to = nrounds, by = 50),
   eta = c(0.025, 0.05, 0.1, 0.3),
   max_depth = c(2, 3, 4, 5, 6),
   gamma = 0,
@@ -190,8 +254,7 @@ tune_control <- caret::trainControl(
   allowParallel = TRUE # FALSE for reproducible results
 )
 
-# We should be using TrainData instead of TestData but we have to include loe_morethan6months in TrainData first
-xgb_tune <- caret::train(loe_morethan6months ~ ., data=testData,
+xgb_tune <- caret::train(loe_morethan6months ~ ., data=trainData2,
                          method='xgbTree',
                          verbose = TRUE,
                          trControl = tune_control,
